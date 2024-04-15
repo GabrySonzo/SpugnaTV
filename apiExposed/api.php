c59c24db89df5b37027a53592624694797ba7ac5
scuola <sonzogni.gabriele.studente@itispaleocapa.it>
2024-04-15 11:08:51 +0000

    token come lo voleva mastro

--- apiExposed/api.php
+++ apiExposed/api.php
@@ -12,9 +12,7 @@
 
     if(isset($uri[3])){
         $type = $uri[3];
-        echo $type;
         $ok = true;
-        echo $_GET["token"];
         if(isset($uri[4])){
             $id = $uri[4];
         }
@@ -38,19 +36,39 @@
                 $query = "SELECT * FROM Film WHERE id = $id";
             }
         }else if($type == "profiles"){
-            $query = "SELECT * FROM Utenti";
-            if($id != null){
-                $query = "SELECT * FROM Utenti WHERE id = $id";
+            if(isset($_GET["token"])){
+                try {
+                    $token = $_GET["token"];
+                    $decoded = (array) JWT::decode($token, new key('mysecret', 'HS256'));
+                    $profile = (array) $decoded["profile"];
+                    if($decoded["role"] == "user"){
+                        $query = "SELECT * FROM Utenti WHERE email = '".$profile["email"]."'";
+                    }else if($decoded["role"] == "admin"){
+                        $query = "SELECT * FROM Utenti";
+                    }
+                } catch (\Exception $e) {
+                    echo json_encode(array("error" => true, "msg" => $e->getMessage()));
+                    $ok = false;
+                }
             }
         }else if($type == "lists"){
             if(isset($_GET["token"])){
                 try {
                     $token = $_GET["token"];
-                    $decoded = JWT::decode($token, new key('mysecret', 'HS256'));
-                    echo $decoded;
-                    $query = "SELECT * FROM Liste";
-                    if($id != null){
-                        $query = "SELECT * FROM Liste WHERE id = $id";
+                    $decoded = (array) JWT::decode($token, new key('mysecret', 'HS256'));
+                    $profile = (array) $decoded["profile"];
+                    if($decoded["role"] == "user"){
+                        if($id != null){
+                            $query = "SELECT * FROM Comprende INNER JOIN Film ON Comprende.film_id = Film.id INNER JOIN Liste ON Comprende.lista_id = Liste.id WHERE lista_id = '".$id."' AND utente_mail = '".$profile["email"]."'";
+                        }else{
+                            $query = "SELECT * FROM Liste WHERE utente_mail = '".$profile["email"]."'";                                
+                        }
+                    }else if($decoded["role"] == "admin"){
+                        if($id != null){
+                            $query = "SELECT * FROM Comprende INNER JOIN Film ON Comprende.film_id = Film.id WHERE lista_id = '".$id."'";
+                        }else{
+                            $query = "SELECT * FROM Liste";
+                        }
                     }
                 } catch (\Exception $e) {
                     echo json_encode(array("error" => true, "msg" => $e->getMessage()));
